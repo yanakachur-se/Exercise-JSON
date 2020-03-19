93bc8019588a5759f96b2f6cb1b9c2600151301c
yanakachur-se <yana.kachur@gmail.com>
2020-03-19 11:01:52 +0100

    Unit tests for MemberService class added

---
+++ src/main/java/edu/sda/parser/MemberJsonIOHandler.java
@@ -0,0 +1,48 @@
+package edu.sda.parser;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.sda.data.JObject;
+import edu.sda.data.Member;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MemberJsonFileHandler {
+
+    public Member[] readValues() {
+        Member[] membersList = null;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            membersList = mapper.readValue(
+                    new File("example-member-list.json"),
+                    JObject.class)
+                    .getMembers();
+
+        } catch (IOException e) {
+            System.out.println(e.getCause());
+        }
+        return membersList;
+    }
+
+    public void saveAttendanceListToFile(Member[] members) {
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(new File("attendance-member-list.json"), members);
+
+        } catch (IOException e) {
+            System.out.println(e.getCause());
+        }
+    }
+
+    public void printPrettyAttendanceList(Member[] members) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(members);
+            System.out.println(json);
+
+        } catch (IOException e) {
+            System.out.println(e.getCause());
+        }
+    }
+}
